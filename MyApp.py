73a7407e707b1745632fdd4e7a35b5cd93a03061
benbowen267 <54160683+benbowen267@users.noreply.github.com>
2021-07-16 13:39:11 +1000

    here

--- MyApp.py
+++ MyApp.py
@@ -1,9 +1,26 @@
 import HomePage
 import Warren
+import Naarm
+import Ngunnawal
+import Nipaluna
+import Gimuy
+import Mparntwe
+import Tarndanya
+import Boorloo
 import streamlit as st
 PAGES = {
     "Home Page": HomePage,
-    "Warren" : Warren
+    "Warren" : Warren,
+    "Naarm": Naarm,
+    "Ngunnawal": Ngunnawal,
+    "Nipaluna": Nipaluna,
+    "Gimuy": Gimuy,
+    "Mparntwe": Mparntwe,
+    "Tarndanya": Tarndanya,
+    "Boorloo": Boorloo,
+    #"Rubibi": Rubibi,
+    #"Garramilla": Garramilla,
+    #"Meanjin": Meanjin
 }
 st.sidebar.title('Navigation Pages')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
