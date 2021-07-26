40c125c9d79033397f589e0bc3aae3ec6e804e2e
Ben Bowen <54160683+benbowen267@users.noreply.github.com>
2021-07-26 14:56:06 +1000

    Update HomePage.py

--- HomePage.py
+++ HomePage.py
@@ -13,8 +13,7 @@ def app():
         st.image('unnamed.png')
 
     with col2:
-        st.title("SHARED PATH ABORIGINAL AND TORRES STRAIIT ISLANDER CORPORATION
-                  ")
+        st.title("SHARED PATH ABORIGINAL AND TORRES STRAIIT ISLANDER CORPORATION")
 #App Name
     st.title('Welcome to the Indigenous Weather Knowledges App')
 #Intro information HomePage
