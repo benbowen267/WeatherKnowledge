bb690cec7186a1b02abd3ad615025d50961a5dcd
Ben Bowen <54160683+benbowen267@users.noreply.github.com>
2021-07-30 11:30:37 +1000

    Update Gabba.py

--- Gabba.py
+++ Gabba.py
@@ -125,10 +125,10 @@ def app():
         content = pd.read_html('https://www.weatherzone.com.au/qld/north-coast-and-tableland/port-douglas', header=0)
         DF= content[0]
         #DF.pop("Weather")
-        DF.pop("Wind")
-        DF.columns = ["Time of Day", "Weather Conditions", "Temperature", "Chance of Rain", "Cloud Cover", "Wind Direction & Speed", "Dew Point", "Humidity"]
-        DF= DF.set_index('Time of Day')
-        DF= DF.iloc[:9,:]
+        #DF.pop("Wind")
+        #DF.columns = ["Time of Day", "Weather Conditions", "Temperature", "Chance of Rain", "Cloud Cover", "Wind Direction & Speed", "Dew Point", "Humidity"]
+        #DF= DF.set_index('Time of Day')
+        #DF= DF.iloc[:9,:]
         return(DF)
         
      
